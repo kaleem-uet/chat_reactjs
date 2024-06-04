6a0e281fcb491f0f149678351507e51e69bd73bf
kaleem-uet <kaleemullahk989@gmail.com>
2024-06-04 12:33:32 +0500

    chat work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import { io } from "socket.io-client";
-const socket = io.connect('http://localhost:3000/');
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
+import socketIO from 'socket.io-client';
 
-import './App.css'
-
+const socket = socketIO.connect('http://localhost:3000');
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <div>
-    <h1>hello world</h1>
-  </div>
-  )
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -3,16 +3,41 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",  // Adjust this to be more restrictive based on your needs
+    methods: ["GET", "POST"]
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-
+let users=[]
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+ //sends the message to all the users on the server
+ socket.on('message', (data) => {
+  io.emit('messageResponse', data);
+});
+
+socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+socket.on('newUser',(data)=>{
+  users.push(data)
+  console.log(users)
+  socket.emit('newUserResponse',users)
+})
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    io.emit('newUserResponse',users)
+    socket.disconnect();
   });
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
